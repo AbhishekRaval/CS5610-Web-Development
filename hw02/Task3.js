d36beee65589baea50fd71a9ff896ef31f53264d
AbhishekRaval <abhishek.raval@gmail.com>
2018-01-21 03:49:11 -0500

    Hw02

---
+++ hw02/Task3.js
@@ -0,0 +1,24 @@
+function showlorem() {
+    document.getElementById('righttable').style.visibility = "visible";
+    var settxt =  document.getElementById('setcontent');
+    var gettxt =  document.getElementById('loremtext');
+    settxt.innerHTML = gettxt.innerHTML
+  }
+  
+
+
+
+function show99bottle(){
+	 document.getElementById('righttable').style.visibility = "visible";
+	  var setcnt = document.getElementById('setcontent');
+	  var getcnt = document.getElementById('bottlestxt');
+	  setcnt.innerHTML = getcnt.innerHTML
+	}
+
+function lastthing1(){
+    document.getElementById('righttable').style.visibility = "visible";
+	var setthing = document.getElementById('setcontent');
+	var getthing = document.getElementById('thirdthing');
+	setthing.innerHTML = getthing.innerHTML
+
+}
